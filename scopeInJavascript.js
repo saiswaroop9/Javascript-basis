e6d72a17634e004df16e7fbcb5fd9a5374eb5023
Sai Swaroop <gujaratisaiswaroop@gmail.com>
2020-07-27 19:34:45 +0530

    Created ScopeUsage

---
+++ scopeInJavascript.js
@@ -0,0 +1,28 @@
+/*
+            Scope's in JavaScript 
+*/
+
+
+let a = 12345;
+console.log(a);
+
+function fn(name) {
+ let i = 9;
+ console.log(i);
+ return `This is a ${name} fn`   
+}
+console.log(fn("Javascript"));
+
+
+
+
+
+let b = 1000;
+console.log(b);
+
+function fn(names) {
+ let j = 9;
+ console.log(j);
+ return `This is a ${names} fn`   
+}
+console.log(fn("Javascript"), b);
